0bdfe73009204f56e61c58ca7664a4f3e2a9adbd
Jadeyn Fincher <jadeyn.fincher@gmail.com>
2025-10-07 17:14:26 -0600

    different cert paths

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use('/api', lootboxRoutes);
 const PORT = process.env.PORT || 3000;
 
 const options = {
-  key: fs.readFileSync('./server.key'),
-  cert: fs.readFileSync('./server.cert'),
+  key: fs.readFileSync('./treggat.us.key'),
+  cert: fs.readFileSync('./treggat.us.cer'),
 };
 
 https.createServer(options, app).listen(PORT, () => {
