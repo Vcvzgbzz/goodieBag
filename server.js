f131335621c31d7043388613a15880123994b273
Jadeyn Fincher <jadeyn.fincher@gmail.com>
2025-10-11 22:56:02 -0600

    Adding version log

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const lootboxRoutes = require('./routes/lootbox');
 const slotRoutes = require('./routes/slots');
+const { version } = require('./package.json');
+
 
 app.use(express.json());
 app.use('/api', lootboxRoutes);
@@ -11,5 +13,9 @@ app.use('/api', slotRoutes);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
+
+
     console.log(`Server running on port ${process.env.PORT || PORT}`);
+    console.log('Current Version of the app: 'version);
+
 });
